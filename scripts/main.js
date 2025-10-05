7c7564f1b1444cec54b45e61d39137f365740d91
Bruna Chaves <brunachavesb@gmail.com>
2025-10-05 17:59:39 -0300

    feat: :sparkles: Adicionando modal do pix e feito mudanças no stilo da página

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,13 @@ const sections = document.querySelectorAll(".section");
 const menuIcon = document.querySelector(".mainNav__icon");
 const navLinks = document.querySelector(".mainNav__links");
 
-links.forEach(link => {
+links.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
 
     const targetId = link.getAttribute("data-section");
 
-    sections.forEach(sec => sec.classList.remove("active"));
+    sections.forEach((sec) => sec.classList.remove("active"));
 
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
@@ -24,4 +24,21 @@ menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+//Modal Pix
+const modal = document.getElementById("pixModal");
+const btn = document.getElementById("btnPix");
+const span = document.querySelector(".close");
 
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
